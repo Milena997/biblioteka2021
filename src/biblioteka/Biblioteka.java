8ca0c501f2c9c3dac592b5573ea820a626deca5f
Milena997 <milenacosovic24@gmail.com>
2021-03-08 23:27:44 +0100

    Dodata je logicka kontriola u klasu biblioteka na drugi nacin

--- src/biblioteka/Biblioteka.java
+++ src/biblioteka/Biblioteka.java
@@ -11,12 +11,20 @@ public class Biblioteka implements BibliotekaInterface {
 
 	@Override
 	public void dodajKnjigu(Knjiga knjiga) {
+		if(knjiga==null) {
+			throw new NullPointerException("ne sme null knjiga");
+		}
+		if(knjige.contains(knjiga)) {
+			throw new RuntimeException("knjiga vec postoji");
+		}
+		
 		knjige.add(knjiga);
 
 	}
 
 	@Override
 	public void obrisiKnjigu(Knjiga knjiga) {
+		
 		knjige.remove(knjiga);
 	}
 
@@ -28,6 +36,9 @@ public class Biblioteka implements BibliotekaInterface {
 
 	@Override
 	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, String isbn, String naslov, String izdavac) {
+		if(autor==null && isbn==null && naslov==null && izdavac==null) {
+			return knjige;
+		}
 		LinkedList<Knjiga> rezultat= new LinkedList<Knjiga>();
 		for(Knjiga knjiga : knjige) {
 			if(knjiga.getNaslov().contains(naslov))
